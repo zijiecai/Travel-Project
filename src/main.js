c48907efedf30eed91ce34b6278533a8930e2354
zijiecai <13005338063@163.com>
2020-10-24 15:50:57 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import fastClick from 'fastclick'
 import store from './store'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+// 注册绑定
+fastClick.attach(document.body);
 
 new Vue({
   router,
