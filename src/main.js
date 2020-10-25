b9171730d6578ccfa6a2c4d67b8489bda51ee88f
zijiecai <13005338063@163.com>
2020-10-25 11:12:50 +0800

    add index-swiper

--- src/main.js
+++ src/main.js
@@ -2,14 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import store from './store'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
 import './assets/styles/iconfont.css'
+import 'swiper/swiper-bundle.css'
 
 Vue.config.productionTip = false
 // 注册绑定
 fastClick.attach(document.body);
+Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
 
 new Vue({
   router,
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOptions">
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt="景点图" />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOptions: {
+        pagination: {
+          el: ".swiper-pagination",
+          type: "bullets",
+          dynamicBullets: true,
+          bulletElement: "span", //设置分页器小圆点标签，默认为span标签
+          // 点击分页器的指示点分页器会控制Swiper切换
+          clickable: true,
+        },
+        loop: true,
+      },
+      swiperList: [
+        {
+          id: "0001",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1801/1a/94428c6dea109402.jpg_640x200_2cf590d8.jpg",
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1802/42/7c92b9a381e46402.jpg_640x200_1cdce2a4.jpg",
+        },
+      ],
+    };
+  },
+  methods: {},
+};
+</script>
+<style lang="stylus" scoped>
+.wrapper
+  // 保证不会产生抖动（图片加载过程中产生的重绘）
+  overflow hidden
+  width 100%
+  height 0
+  padding-bottom 31.25%
+  .swiper-img
+    width 100%
+</style>
